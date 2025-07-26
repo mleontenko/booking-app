6d90110510aa58709a8261a08931f21e4c4a8bd5
Marin Leontenko <marin.leontenko@ses.com>
2025-07-26 17:05:23 +0200

    layout

--- main.py
+++ main.py
@@ -3,13 +3,32 @@ import pandas
 df = pandas.read_csv("hotels.csv")
 
 class Hotel():
+    def __init__(self, id):
+        pass        
+
     def book(self):
+        pass
+
+    def available(self):
         pass
 
 
 class ReservationTicket():
-    def generate(self):
+    def __init__(self, vustomer_name, hotel_object):
         pass
+
+    def generate(self):
+        content= f"Name of the customer hotel"
+        print(content)
 
 
 print(df)
+id = input("Enter the id of the hotel: ")
+hotel = Hotel(id)
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationTicket(name, hotel)
+    print(reservation_ticket.generate())
+else:
+    print("Hotel is not free.")
