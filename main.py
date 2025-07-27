d0ae800abde4c2527ba18c1869b04764e75d1e4c
Marin Leontenko <marin.leontenko@ses.com>
2025-07-27 10:09:40 +0200

    add Hotel class methods

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ df = pandas.read_csv("hotels.csv", dtype={"id": str})
 
 class Hotel():
     def __init__(self, hotel_id):
-        self.hotel_id = hotel_id        
+        self.hotel_id = hotel_id       
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
 
     def book(self):
         """
@@ -27,20 +28,26 @@ class Hotel():
 
 class ReservationTicket():
     def __init__(self, customer_name, hotel_object):
-        pass
+        self.customer_name = customer_name
+        self.hotel = hotel_object
 
     def generate(self):
-        content= f"Name of the customer: hotel:"
-        print(content)
+        content= f"""
+        Thank you for your reservation!
+        Here is your booking data:
+        Name: {self.customer_name}
+        Hotel name: {self.hotel.name}
+        """
+        return content
 
 
 print(df)
-hotel_id = input("Enter the id of the hotel: ")
-hotel = Hotel(hotel_id)
+hotel_ID = input("Enter the id of the hotel: ")
+hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(name, hotel)
+    reservation_ticket = ReservationTicket(customer_name = name, hotel_object = hotel)
     print(reservation_ticket.generate())
 else:
     print("Hotel is not free.")
